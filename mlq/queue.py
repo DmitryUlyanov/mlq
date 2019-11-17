48790a6d6f33f760e79529632d3c308fd3af06ab
Dmitry Ulyanov <dmitry.ulyanov.msu@gmail.com>
2019-11-16 21:26:09 -0800

    Update queue.py

--- mlq/queue.py
+++ mlq/queue.py
@@ -18,21 +18,7 @@ import jsonpickle
 import cloudpickle
 
 
-logging.basicConfig(level=logging.DEBUG)
-
-
-# create console handler and set level to debug
-ch = logging.StreamHandler()
-ch.setLevel(logging.DEBUG)
-# create formatter
-formatter = logging.Formatter(
-    '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-# add formatter to ch
-ch.setFormatter(formatter)
-# add ch to logger
-logger.addHandler(ch)
-
-
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG)
 
 class MLQ():
     """Create an MLQ object"""
